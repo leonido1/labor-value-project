d2c7eef0cf7af08cf91a3125b0a1b91722d45fda
Leonid Vishniakov <leonidvishniakov@gmail.com>
2022-02-21 19:34:13 +0200

    table

---
+++ src/components/AddProduct.js
@@ -0,0 +1,23 @@
+import React, {Component, component} from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import ProductInputTable from "./productInputTable"
+
+export default class AddProduct extends Component{
+
+
+
+
+
+
+
+    render(){
+      return ( <ProductInputTable></ProductInputTable> );
+    }
+
+
+}
+
+
+
+
+
