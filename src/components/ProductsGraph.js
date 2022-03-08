d6b5398d5db80ba598aa2016b027752b39d0beeb
leonido1 <leonidvishniakov@gmail.com>
2022-03-08 19:39:29 +0200

    fixed cycles bug

--- src/components/ProductsGraph.js
+++ src/components/ProductsGraph.js
@@ -151,6 +151,7 @@ export default class ProductsGraph extends Component{
       <Labourvalue key={productName} name={productName} ></Labourvalue >
 
       <br></br>
+            
       <Typeahead
           id="select-product-name"
           labelKey="name"
